ea47fe2a4c82d756965c306f8a2d860887610dd1
Lars Hedlund <lkhedlund@gmail.com>
2016-01-16 09:54:15 -0800

    Some improvements to code layout

--- flashcard/FlashcardApp.js
+++ flashcard/FlashcardApp.js
@@ -9,22 +9,16 @@ MAIN FEATURES:
 
 BUGS: None known
 VERSION: 1.0
-
 */
-
-// card tracking
-TOTAL = cards.length;
-
-// Holds all of the students' answers to be displayed at the end of the quiz.
-var student_answers = [];
 
 // Start of the main application
 var FlashcardApp = FlashcardApp || {};
 
 FlashcardApp.init = function() {
-    // Initialize 'last' question at -1 to start with a fresh question.
-    last = -1;
-    FlashcardApp.load_card();
+    // Holds all of the students' answers to be displayed at the end of the quiz.
+    var student_answer = [];
+    var current = 0;
+    FlashcardApp.load_card(current);
 };
 
 FlashcardApp.refresh = function() {
@@ -32,7 +26,7 @@ FlashcardApp.refresh = function() {
     $('#flashcard').html('');
 };
 
-FlashcardApp.load_card = function() {
+FlashcardApp.load_card = function(current) {
     /* METHOD: Loads a random flashcard question from any array of varying
      length. The array must be organized as an 'associative array', where the
      outer braces [] contain objects {} with key pairs.
@@ -46,36 +40,47 @@ FlashcardApp.load_card = function() {
     // Change these variables if you change the div ids
     var $display_card = $('#flashcard');
 
+    // Store total card length in a constant
+    var TOTAL = cards.length;
+
     // Refresh content
     FlashcardApp.refresh();
 
+    // Initialize 'last' question at -1 to start with a fresh question.
+    // var last = -1;
     // Selects a random question
-    current = Math.floor(Math.random() * TOTAL);
-    if (current == last) {
-        current = Math.floor(Math.random() * TOTAL);
-    };
+    // var current = Math.floor(Math.random() * TOTAL);
+    // if (current === last) {
+    //     current = Math.floor(Math.random() * TOTAL);
+    // };
     var current_card = cards[current];
     // Loads a new question
     $display_card.append(current_card["question"]);
     $display_card.attr('value','question');
     // Ensure that the same question does not come up twice in a row.
-    last = current;
+    // last = current;
+
+    // Wait for user input to move on
+    $('button').on('click', function() {
+        var id_value = this.id;
+        FlashcardApp.flip_card(current, id_value);
+    });
 };
 
-FlashcardApp.flip_card = function(id_value) {
+FlashcardApp.flip_card = function(current, id_value) {
     /* METHOD: Flip the card over to show the answer and/or reshow the question.
     Only changes once the user chooses next.
     INPUT: Button id for easier comparison in if/else statements
     */
 
     // Change these variables if you change the div ids
-    var $display_card = $('#flashcard');
-    var current_card = cards[current];
-    var card_value = $display_card.attr('value');
+    var $display_card = $('#flashcard'),
+        current_card = cards[current],
+        card_value = $display_card.attr('value');
 
     // Start of button logic checks.
-    if (id_value == 'flip') {
-        if (card_value == 'question') {
+    if (id_value === 'flip') {
+        if (card_value === 'question') {
             // Refresh content
             FlashcardApp.refresh();
             // Load side
@@ -88,8 +93,9 @@ FlashcardApp.flip_card = function(id_value) {
             $display_card.append(current_card["question"]);
             $display_card.attr('value','question');
         };
-    } else {
+    };
+    if (id_value === 'next') {
         // Next is the only other option, so pull a new card.
-        FlashcardApp.load_card();
+        FlashcardApp.load_card(current + 1);
     };
 };
