3cf47f514622c61034b6d02f43e02c63a9f9d478
Lars Hedlund <lkhedlund@gmail.com>
2016-01-30 13:49:33 -0800

    Added fallback for missing cards file or array

--- flashcard/FlashcardApp.js
+++ flashcard/FlashcardApp.js
@@ -7,19 +7,35 @@ MAIN FEATURES:
 (3) User can skip or randomly move through cards at their own pace.
 (4) Card will load 'indefinitely', including repeats.
 BUGS: None known
-VERSION: 1.2
+
+VERSION: 1.3
 */
 
 $(function() {
 
-  /* All cards are loaded from a global cards array contained in cards.js.
+  /*
   The clone action allows the user to loop through one random set of cards before starting over.
   */
-  var cloned_cards = cloneCards();
 
-  init();
+  // Rename cards.js to the name of the file and path.
+  var cards_location = 'cards.js';
+
+  try {
+    init();
+  } catch (e) {
+    $('body').append(e + ". The cards file may be missing or invalid.")
+    if (e instanceof ReferenceError) {
+      // fallback if the script containing the cards is missing
+      $.getScript(cards_location, function() {
+        init();
+      });
+    } else {
+      console.log(e);
+    }
+  }
 
   function init() {
+    cloned_cards = cloneCards();
     loadCard();
   }
 
@@ -27,11 +43,6 @@ $(function() {
     /* Creates a copy of the cards so that the user gets one of each card
     before starting over. */
     return JSON.parse(JSON.stringify(cards));
-  }
-
-  function refresh() {
-    // Refreshes div ids that the flashcard is written to
-    $('#flashcard').html('');
   }
 
   function loadCard() {
@@ -46,10 +57,7 @@ $(function() {
     */
 
     // Change this if you change the div ids
-    var $display_card = $('#flashcard');
-
-    // Refresh content
-    refresh();
+    var display_card = $('#flashcard');
 
     // Selects a random question
     if (cloned_cards.length === 0) {
@@ -61,8 +69,8 @@ $(function() {
     current_card = cloned_cards.splice(Math.floor(Math.random() * cloned_cards.length), 1)[0];
 
     // Loads a new question
-    $display_card.append(current_card["question"]);
-    $display_card.attr('value','question');
+    display_card.html(current_card["question"]);
+    display_card.attr('value','question');
   }
 
   function flipCard(id_value) {
@@ -71,23 +79,19 @@ $(function() {
     */
 
     // Change these variables if you change the div ids
-    var $display_card = $('#flashcard'),
-        card_value = $display_card.attr('value');
+    var display_card = $('#flashcard'),
+        card_value = display_card.attr('value');
 
     // Start of button logic checks.
     if (id_value === 'flip') {
         if (card_value === 'question') {
-            // Refresh content
-            refresh();
             // Load side
-            $display_card.append(current_card["answer"]);
-            $display_card.attr('value','answer');
+            display_card.html(current_card["answer"]);
+            display_card.attr('value','answer');
         } else {
-            // Refresh content
-            refresh();
             // Load side
-            $display_card.append(current_card["question"]);
-            $display_card.attr('value','question');
+            display_card.html(current_card["question"]);
+            display_card.attr('value','question');
         }
     }
     if (id_value === 'next') {
