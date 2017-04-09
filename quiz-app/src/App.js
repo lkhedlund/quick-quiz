8f62dfbf27793bf598568ea46ac8084fc40c2e4c
Lars Hedlund <lkhedlund@gmail.com>
2017-04-09 10:39:02 -0700

    Refactoring App and Quiz wrapper

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import MultipleChoiceQuiz from './components/MultipleChoiceQuiz.js';
-import EXERCISES from './exercises/exercise_1.js';
 import './App.css';
+import QuizQuestion from './components/QuizQuestion';
+import QuizOption from './components/QuizOption';
+import QuizButton from './components/QuizButton';
+import EXERCISES from './exercises/exercise_1.js';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionNumber: 0,
+    };
+  }
+  render () {
+    let currentQuestion = EXERCISES[this.state.questionNumber];
+    let options = [];
+    currentQuestion.options.forEach(function(option) {
+      options.push(<QuizOption value={option} key={option}/>);
+    });
+    let answer = currentQuestion.answer;
+    options.push(<QuizOption value={answer} key={answer} />);
+
+    let question = currentQuestion.question;
     return (
-      <div className="App">
-        <MultipleChoiceQuiz exercises={EXERCISES[0]} />
+      <div>
+        <QuizQuestion question={question} />
+        <div className="options">
+          {options}
+        </div>
+        <QuizButton name="Submit" />
       </div>
     );
   }
