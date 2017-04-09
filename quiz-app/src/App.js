c82ccb1a023ac79142200e286d56475c29a5b03d
Lars Hedlund <lkhedlund@gmail.com>
2017-04-09 10:24:20 -0700

    Moved exercises into separate file

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,38 +1,10 @@
 import React, { Component } from 'react';
 import MultipleChoiceQuiz from './components/MultipleChoiceQuiz.js';
+import EXERCISES from './exercises/exercise_1.js';
 import './App.css';
 
 class App extends Component {
   render() {
-    const EXERCISES = [
-      {
-        "question" : "What is your <i>name</i>?",
-        "answer" : "King Arthur of Camelot",
-        "options" : [
-          "Sir Gawain",
-          "Dennis",
-          "Roger the Shrubber"
-        ]
-      },
-      {
-        "question" : "What is your <b>quest</b>?",
-        "answer" : "To seek the holy grail",
-        "options" : [
-          "I don't know",
-          "Really small rocks",
-          "Coconuts"
-        ]
-      },
-      {
-        "question" : "What is the airspeed velocity of an unladen swallow?",
-        "answer" : "African or European?",
-        "options" : [
-          "42",
-          "I don't know",
-          "Yellow"
-        ]
-      }
-    ];
     return (
       <div className="App">
         <MultipleChoiceQuiz exercises={EXERCISES[0]} />
---
+++ quiz-app/src/exercises/exercise_1.js
@@ -0,0 +1,31 @@
+const EXERCISES = [
+  {
+    "question" : "What is your <i>name</i>?",
+    "answer" : "King Arthur of Camelot",
+    "options" : [
+      "Sir Gawain",
+      "Dennis",
+      "Roger the Shrubber"
+    ]
+  },
+  {
+    "question" : "What is your <b>quest</b>?",
+    "answer" : "To seek the holy grail",
+    "options" : [
+      "I don't know",
+      "Really small rocks",
+      "Coconuts"
+    ]
+  },
+  {
+    "question" : "What is the airspeed velocity of an unladen swallow?",
+    "answer" : "African or European?",
+    "options" : [
+      "42",
+      "I don't know",
+      "Yellow"
+    ]
+  }
+];
+
+export default EXERCISES;
